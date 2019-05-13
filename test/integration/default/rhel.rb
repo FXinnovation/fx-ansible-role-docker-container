fa6c5d9a00b98887108bcfcd15352d4693351203
PHELEP Gwenael <gwenael.phelep@fxinnovation.com>
2019-05-13 16:49:28 -0400

    Modification du test du container pour Centos et Redhat

--- test/integration/default/rhel.rb
+++ test/integration/default/rhel.rb
@@ -1,7 +1,11 @@
 control "ansible-role-docker-container - #{os.name} #{os.release} - 01" do
   impact 1.0
   title 'Ansible role docker container'
-  describe package('docker-ce') do
-    it { should be_installed }
+  describe docker_container(name: 'application') do
+  it { should exist }
+  it { should be_running }
+  its('repo') { should eq 'nginx' }
+  its('ports') { should eq "80/tcp, 0.0.0.0:24224->24224/tcp" }
+  its('command') { should eq "nginx -g 'daemon off;'" }
   end
 end
